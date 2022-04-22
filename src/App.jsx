51694081a2d373f4b93ba3e002ccae954594ad64
sivagurubilli <gurusrija123@gmail.com>
2022-04-22 18:21:38 +0530

    project start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     
+        hi
+    </div>
+  )
+}
+
+export default App
